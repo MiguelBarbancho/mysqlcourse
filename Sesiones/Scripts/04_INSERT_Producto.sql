9ac2d9b59a043c7c3474b32912ced077841b5ad4
A\ghernandez <geohernandez@gmail.com>
2018-06-08 09:34:37 +0200

    Last version

---
+++ Sesiones/Scripts/04_INSERT_Producto.sql
@@ -0,0 +1,18 @@
+USE gpr;
+
+INSERT INTO Producto  ( IdProducto, Nombre, FechaRegistro, IdEstado)
+VALUES
+( 1, 'Enfrijoladas', N'2003-11-01T00:00:00', 1 ), 
+( 2, 'Sopa de tortilla', N'2000-09-18T00:00:00', 2 ), 
+( 3, 'Frijoles refritos', N'2002-03-16T00:00:00', 3 ), 
+( 4, 'Chilaquiles', N'2004-01-28T00:00:00', 4 ), 
+( 5, 'Tamales', N'2005-06-10T00:00:00', 5 ), 
+( 6, 'Tostadas', N'2002-03-21T00:00:00', 6 ), 
+( 7, 'Sopa de tortilla', N'2002-04-05T00:00:00', 7 ), 
+( 8, 'Picadillo', N'2001-12-14T00:00:00', 8 ), 
+( 9, 'Frijoles refritos', N'2005-09-12T00:00:00', 9 ), 
+( 10, 'Tostadas', N'2006-07-24T00:00:00', 10 ), 
+( 11, 'Mole poblano', N'2000-06-29T00:00:00', 11 ), 
+( 12, 'Pozole', N'2005-02-17T00:00:00', 12 ), 
+( 13, 'Pozole', N'2004-11-07T00:00:00', 13 ), 
+( 14, 'Pozole', N'2003-07-13T00:00:00', 14 );
